0499b01538d2cfa5a857a7192a2734e0f4b5e7cc
Williams25 <william.souza.timao@hotmail.com>
2022-06-11 16:01:10 -0300

    ajustando card adicionando formatação aos valores

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,30 +1,48 @@
+import { Button } from "components/Button";
 import Image from "next/image";
 import styled from "./styles.module.scss";
+import { MdShoppingCart } from "react-icons/md";
+import { Products } from "types/Products";
+import { formatCurrency } from "utils/currency";
 
-export const ProductCard = () => {
+export type ProductCardProps = Pick<
+  Products,
+  "image" | "name" | "price" | "oldPrice"
+>;
+
+export const ProductCard = ({
+  image,
+  name,
+  price,
+  oldPrice
+}: ProductCardProps) => {
   return (
     <div className={styled.cardContainer}>
       <div className={styled.boxProduct}>
         <div className={styled.imageBox}>
-          <Image
-            src="https://i.imgur.com/ExJ8WOq.jpg"
-            alt=""
-            width={368}
-            height={368}
-          />
+          <Image src={image} alt={name} width={368} height={368} />
         </div>
         <div className={styled.info}>
-          <h1>
-            Processador AMD Ryzen 7 5700G, 3.8GHz (4.6GHz Max Turbo), AM4, Vídeo
-            Integrado, 8 Núcleos - 100-100000263BOX
-          </h1>
-          <span className={styled.oldPriceCard}>R$ 5,14</span>
-          <h2>R$ 7,99</h2>
+          <h1>{name}</h1>
+          {!!oldPrice && (
+            <span className={styled.oldPriceCard}>
+              {formatCurrency(oldPrice)}
+            </span>
+          )}
+          <h2>{formatCurrency(price)}</h2>
           <span className={styled.priceTextCard}>À vista no PIX</span>
         </div>
       </div>
 
-      <button>teste</button>
+      <Button
+        title="Comprar"
+        variant="button-dark"
+        size="small"
+        icon={<MdShoppingCart />}
+        positionIcon="left"
+        fullWidth
+        type="button"
+      />
     </div>
   );
 };
