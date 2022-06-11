b3c28911ae136921a4dbaf7fb9125804f133d264
Williams25 <william.souza.timao@hotmail.com>
2022-06-11 18:19:54 -0300

    adicionando animação na grid de produtos

--- src/components/ProductGrid/index.tsx
+++ src/components/ProductGrid/index.tsx
@@ -1,16 +1,43 @@
 import styled from "./styles.module.scss";
 import { ProductCard } from "components/ProductCard";
-import prod from "../../../prod.json";
+import { Products } from "types/Products";
+import { motion, AnimatePresence } from "framer-motion";
 
-export const ProductGrid = () => {
+export type ProductGridProps = {
+  products: Products[];
+};
+
+export const ProductGrid = ({ products }: ProductGridProps) => {
   return (
     <section className={styled.sectionProducts}>
       <div className={styled.contentGridProduct}>
-        <div className={styled.products}>
-          {prod.map((product) => (
-            <ProductCard key={product.id} {...product} />
-          ))}
-        </div>
+        <motion.div className={styled.products}>
+          <AnimatePresence>
+            {products.map((product) => (
+              <motion.div
+                key={product.id}
+                layout
+                animate={{
+                  opacity: 1,
+                  scale: 1
+                }}
+                initial={{
+                  opacity: 0,
+                  scale: 0
+                }}
+                exit={{
+                  opacity: 0,
+                  scale: 0
+                }}
+                transition={{
+                  duration: 0.3
+                }}
+              >
+                <ProductCard {...product} />
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </motion.div>
       </div>
     </section>
   );
