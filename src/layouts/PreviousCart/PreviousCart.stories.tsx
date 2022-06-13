9af13145c3d27d2ce0afe7b84d20177e272c2ed5
Williams25 <william.souza.timao@hotmail.com>
2022-06-12 21:47:40 -0300

    criando preview do precarrinho

---
+++ src/layouts/PreviousCart/PreviousCart.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, Story } from "@storybook/react/types-6-0";
+import { PreviousCart, PreviousCartProps } from "./index";
+import prod from "../../../prod.json";
+
+export default {
+  title: "Pages/PreviousCart",
+  component: PreviousCart,
+  parameters: {
+    layout: "fullscreen",
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/
+      }
+    }
+  },
+  args: {
+    products: [prod[0]]
+  }
+} as Meta<PreviousCartProps>;
+
+export const Basic: Story<PreviousCartProps> = (args) => (
+  <PreviousCart {...args} />
+);
