92011b9d3f6254ca498d41319cfdb23f8eb6860b
Williams25 <william.souza.timao@hotmail.com>
2022-06-13 22:38:42 -0300

    refatorando home e repassando produtos via getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
-import { ProductContext } from "context/ProductProvider";
-import { useContext } from "react";
 import { Home } from "layouts/Home";
+import type { GetStaticProps } from "next";
+import { Products } from "types/Products";
+import { productService } from "services/products";
 
-const HomePage = () => {
-  const { products } = useContext(ProductContext);
+export type HomePageProps = {
+  products: Products[];
+};
 
+const HomePage = ({ products }: HomePageProps) => {
   return <Home products={products} />;
+};
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+  const { data } = await productService.getAll();
+
+  return {
+    props: {
+      products: data
+    },
+    revalidate: 200
+  };
 };
 
 export default HomePage;
