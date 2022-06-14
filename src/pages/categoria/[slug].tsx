aaa34562fc515b11a13b797bbb8a04cd3175d60d
Williams25 <william.souza.timao@hotmail.com>
2022-06-13 22:38:12 -0300

    criando pagina de categoria

---
+++ src/pages/categoria/[slug].tsx
@@ -0,0 +1,57 @@
+import { Home } from "layouts/Home";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { Params } from "pages/pre-carrinho/[slug]";
+import { productService } from "services/products";
+import { Products } from "types/Products";
+
+export type CategoriePageProps = {
+  products: Products[];
+};
+
+const CategoriePage = ({ products }: CategoriePageProps) => {
+  return <Home products={products} />;
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const paths = [
+    {
+      params: { slug: "Gamer" }
+    },
+    {
+      params: { slug: "Monitores" }
+    },
+    {
+      params: { slug: "Hardware" }
+    },
+    {
+      params: { slug: "Celular" }
+    },
+    {
+      params: { slug: "Áudio" }
+    }
+  ];
+
+  return {
+    paths,
+    fallback: "blocking"
+  };
+};
+
+export const getStaticProps: GetStaticProps<CategoriePageProps> = async (
+  ctx
+) => {
+  const { slug } = ctx.params as Params;
+
+  const { data } = await productService.getAll();
+
+  const products = data.filter((p) => p.categorie === slug);
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 200
+  };
+};
+
+export default CategoriePage;
