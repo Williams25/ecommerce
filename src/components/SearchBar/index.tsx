d959b5b1d5b685d0f013a7d67f2f28b4a9bdb94e
Williams25 <william.souza.timao@hotmail.com>
2022-06-13 22:32:46 -0300

    refatorando a barra de pesquisa

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,37 +1,54 @@
-import { Dispatch, SetStateAction } from "react";
+import { useContext, useState } from "react";
 import { RiSearch2Line } from "react-icons/ri";
+import { Combobox } from "@headlessui/react";
 import styled from "./styles.module.scss";
+import { ProductContext } from "context/ProductProvider";
+import { ProductSearchCard } from "components/ProductSearchCard";
 
 export type SearchBarProps = {
   type: "name" | "categorie";
-  searchProduct: string;
-  setSearchProduct: Dispatch<SetStateAction<string>>;
   // eslint-disable-next-line no-unused-vars
   handleSearch: (search: string, target?: "name" | "categorie") => void;
 };
 
-export const SearchBar = ({
-  handleSearch,
-  setSearchProduct,
-  type = "name",
-  searchProduct
-}: SearchBarProps) => {
+export const SearchBar = ({ handleSearch, type = "name" }: SearchBarProps) => {
+  const { products } = useContext(ProductContext);
+  const [searchProduct, setSearchProduct] = useState<string>("");
+
   return (
-    <div className={styled.searchContent}>
-      <div className={styled.searchBar}>
-        <input
-          type="text"
-          placeholder="Busque aqui"
-          onChange={(e) => {
-            setSearchProduct(e.target.value);
-            handleSearch(e.target.value, type);
-          }}
-        />
-        <button type="button" onClick={() => handleSearch(searchProduct, type)}>
-          <RiSearch2Line />
-        </button>
+    <Combobox value={searchProduct} onChange={setSearchProduct}>
+      <div className={styled.searchContent}>
+        <div className={styled.searchBar}>
+          <Combobox.Input
+            type="text"
+            placeholder="Busque aqui"
+            onChange={(e) => {
+              setSearchProduct(e.target.value);
+              handleSearch(e.target.value, type);
+            }}
+          />
+          <Combobox.Button
+            type="button"
+            onClick={() => handleSearch(searchProduct, type)}
+          >
+            <RiSearch2Line />
+          </Combobox.Button>
+        </div>
+
+        <Combobox.Options className={styled.papper}>
+          <div className={styled.resultItems}>
+            {products.map((item) => (
+              <Combobox.Option key={item.id} value={item.name}>
+                <ProductSearchCard
+                  product={{ ...item }}
+                  search={searchProduct}
+                  type={type}
+                />
+              </Combobox.Option>
+            ))}
+          </div>
+        </Combobox.Options>
       </div>
-      {/* fazer o painel de busca */}
-    </div>
+    </Combobox>
   );
 };
