c72744ccb7e58d3b4f259ef10675d93e2d3e9cf4
Williams25 <william.souza.timao@hotmail.com>
2022-06-14 21:26:25 -0300

    criando pagina 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { NotFoundException } from "layouts/NotFoundException";
+
+const NotFoundExceptionPage = () => {
+  return (
+    <>
+      <NotFoundException />
+    </>
+  );
+};
+
+export default NotFoundExceptionPage;
