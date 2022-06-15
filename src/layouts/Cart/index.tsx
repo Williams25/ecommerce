48d14d489f3dcd09543e4567a8ea7e35fc1d9062
Williams25 <william.souza.timao@hotmail.com>
2022-06-14 21:22:00 -0300

    adicionando validação para carrinho vazio

--- src/layouts/Cart/index.tsx
+++ src/layouts/Cart/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "components/Header";
 import { ProductInCart } from "components/ProductInCart";
 import { CartContext } from "context/CartProvider";
+import Image from "next/image";
+import Link from "next/link";
 import { useContext } from "react";
 import styled from "./styles.module.scss";
 
@@ -19,6 +21,23 @@ export const Cart = () => {
                 product={{ ...product }}
                 type="cart"
               />
+            ))}
+
+          {!productsCart ||
+            (productsCart.length === 0 && (
+              <div className={styled.badCart}>
+                <Image
+                  src="/cart-bad.svg"
+                  alt="imagem representa um carrinho vazio"
+                  width={100}
+                  height={100}
+                />
+
+                <h1>Seu carrinho está vazio!</h1>
+                <Link href="/">
+                  <a>volte para home e adicione produtos em seu carrinho!</a>
+                </Link>
+              </div>
             ))}
         </div>
       </section>
