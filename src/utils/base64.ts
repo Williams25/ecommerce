be9fe83b9b0eeb89ecfc874f7d3905924fd40b54
Williams25 <william.souza.timao@hotmail.com>
2022-06-14 21:26:41 -0300

    ajustando função base64

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,7 +1,7 @@
 import { encode, decode } from "js-base64";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const encoded = (item: any): string => encode(String(item));
+export const encoded = (item: any): string => encode(JSON.stringify(item));
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const decoded = (item: any): string => decode(String(item));
+export const decoded = (item: any): string => decode(JSON.stringify(item));
