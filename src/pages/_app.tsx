54e1f5f6df52a57c790d04f4aa09d480dace7010
Williams25 <william.souza.timao@hotmail.com>
2022-06-16 20:44:36 -0300

    removendo dependencias não utilizadas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,21 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { QueryClientProvider, Hydrate } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { queryClient } from "services/queryClient";
 import { ProductProvider } from "context/ProductProvider";
 import Head from "next/head";
 import { CartProvider } from "context/CartProvider";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Head>
         <title>Desafio OSF</title>
       </Head>
-      <ReactQueryDevtools initialIsOpen position="bottom-right" />
-      <Hydrate state={pageProps.dehydratedState}>
-        <ProductProvider>
-          <CartProvider>
-            <Component {...pageProps} />
-          </CartProvider>
-        </ProductProvider>
-      </Hydrate>
-    </QueryClientProvider>
+      <ProductProvider>
+        <CartProvider>
+          <Component {...pageProps} />
+        </CartProvider>
+      </ProductProvider>
+    </>
   );
 };
 
