73c0c5078949ccfb919f2dd3b1b7f6f5a2028c32
Williams25 <william.souza.timao@hotmail.com>
2022-06-15 21:28:16 -0300

    ajustando carrinho adicionando checkout e reajustando grid

--- src/layouts/Cart/index.tsx
+++ src/layouts/Cart/index.tsx
@@ -1,13 +1,21 @@
+import { CheckOrder } from "components/CheckOrder";
 import { Header } from "components/Header";
 import { ProductInCart } from "components/ProductInCart";
 import { CartContext } from "context/CartProvider";
 import Image from "next/image";
 import Link from "next/link";
 import { useContext } from "react";
+import { calcPorcent } from "utils/calcPorcent";
 import styled from "./styles.module.scss";
 
 export const Cart = () => {
-  const { productsCart } = useContext(CartContext);
+  const {
+    productsCart,
+    handleTopayOrder,
+    handleTotalProducts,
+    activeCoupons,
+    verifyCoupon
+  } = useContext(CartContext);
   return (
     <div className={styled.containerCart}>
       <Header />
@@ -22,24 +30,40 @@ export const Cart = () => {
                 type="cart"
               />
             ))}
-
-          {!productsCart ||
-            (productsCart.length === 0 && (
-              <div className={styled.badCart}>
-                <Image
-                  src="/cart-bad.svg"
-                  alt="imagem representa um carrinho vazio"
-                  width={100}
-                  height={100}
-                />
-
-                <h1>Seu carrinho está vazio!</h1>
-                <Link href="/">
-                  <a>volte para home e adicione produtos em seu carrinho!</a>
-                </Link>
-              </div>
-            ))}
         </div>
+
+        {!productsCart ||
+          (productsCart.length === 0 && (
+            <div className={styled.badCart}>
+              <Image
+                src="/cart-bad.svg"
+                alt="imagem representa um carrinho vazio"
+                width={100}
+                height={100}
+              />
+
+              <h1>Seu carrinho está vazio!</h1>
+              <Link href="/">
+                <a>volte para home e adicione produtos em seu carrinho!</a>
+              </Link>
+            </div>
+          ))}
+
+        {productsCart && productsCart.length > 0 && (
+          <CheckOrder
+            order={{
+              toPay: handleTopayOrder,
+              totalProducts: handleTotalProducts,
+              valueProducts: handleTopayOrder,
+              discount: calcPorcent(
+                activeCoupons?.discount || 0,
+                handleTopayOrder
+              )
+            }}
+            activeCoupons={activeCoupons}
+            verifyCoupon={verifyCoupon}
+          />
+        )}
       </section>
     </div>
   );
