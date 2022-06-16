668af89363e148033c68ce7689d2f7a4a9a4d48d
Williams25 <william.souza.timao@hotmail.com>
2022-06-15 21:24:33 -0300

    criando componente para finalizar a compra

---
+++ src/components/CheckOrder/index.tsx
@@ -0,0 +1,82 @@
+import styled from "./styles.module.scss";
+import { formatCurrency } from "utils/currency";
+import { Button } from "components/Button";
+import { useState } from "react";
+import { Coupon } from "types/Coupon";
+
+export type CheckOrderProps = {
+  order: {
+    totalProducts: number;
+    valueProducts: number;
+    toPay: number;
+    discount?: number;
+  };
+  // eslint-disable-next-line no-unused-vars
+  verifyCoupon: (coupon: string) => void;
+  activeCoupons: Coupon | null;
+};
+
+export const CheckOrder = ({
+  order,
+  verifyCoupon,
+  activeCoupons
+}: CheckOrderProps) => {
+  const [couponText, setCouponText] = useState<string>("");
+  return (
+    <div className={styled.checkOrderContent}>
+      <h1>Resumo do pedido</h1>
+
+      <ul>
+        <li>
+          Total de produtos <span>({order.totalProducts}) itens</span>
+        </li>
+        <li>
+          Valor dos Produtos: <span>{formatCurrency(order.valueProducts)}</span>
+        </li>
+        <li>
+          Frete:{" "}
+          <span>
+            {order.toPay - (order?.discount || 0) > 1000
+              ? "Grátis"
+              : formatCurrency(10)}
+          </span>
+        </li>
+        <li>
+          Valor total:{" "}
+          <span>
+            {formatCurrency(
+              order.discount ? order.toPay - order.discount : order.toPay
+            )}
+          </span>
+        </li>
+      </ul>
+
+      <div className={styled.contentInput}>
+        <input
+          type="text"
+          placeholder="Cupom de desconto"
+          value={couponText}
+          onChange={(e) => setCouponText(e.target.value)}
+        />
+
+        <Button
+          title="aplicar"
+          variant="button-light"
+          size="small"
+          type="button"
+          onClick={() => verifyCoupon(couponText)}
+        />
+      </div>
+      {activeCoupons?.discount && (
+        <span className={styled.couponValid}>Cupom aplicado com sucesso!</span>
+      )}
+
+      <Button
+        fullWidth
+        title="Finalizar Compra"
+        variant="button-dark"
+        size="medium"
+      />
+    </div>
+  );
+};
