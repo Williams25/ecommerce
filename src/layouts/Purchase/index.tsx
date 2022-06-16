388bb135caa2dc841e1304d8c3ca288166ee8d37
Williams25 <william.souza.timao@hotmail.com>
2022-06-16 20:41:15 -0300

    criando pagina de compra finalizada

---
+++ src/layouts/Purchase/index.tsx
@@ -0,0 +1,19 @@
+import { Header } from "components/Header";
+import Link from "next/link";
+import styled from "./styles.module.scss";
+
+export const Purchase = () => {
+  return (
+    <>
+      <Header />
+
+      <div className={styled.contentPurchase}>
+        <h1>Compra finalazada com sucesso</h1>
+
+        <Link href="/">
+          <a>Voltar para home</a>
+        </Link>
+      </div>
+    </>
+  );
+};
