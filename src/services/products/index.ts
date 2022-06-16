33ea2f8fd01c90f75167cf18224a510bd4df45e9
Williams25 <william.souza.timao@hotmail.com>
2022-06-15 21:26:51 -0300

    adicionando serviço para recuperar os cupons da api

--- src/services/products/index.ts
+++ src/services/products/index.ts
@@ -1,8 +1,12 @@
 import { http } from "services/axios_config";
 import { Products } from "types/Products";
+import { Coupon } from "types/Coupon";
 
 export const productService = {
   getAll: () => {
     return http.get<Products[]>(`/products`);
   },
+  getAllCoupons: () => {
+    return http.get<Coupon[]>(`/coupon`);
+  }
 };
