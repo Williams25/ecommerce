75cd3878f8ccaf0d86db94d1a5102be46c717510
Williams25 <william.souza.timao@hotmail.com>
2022-06-15 21:26:02 -0300

    ajustando hook de pesquisa removendo pesquisa realizada sobre o mock de produtos e ajustando para lista de produtos retornados da api

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,25 +1,24 @@
 import { ProductContext } from "context/ProductProvider";
 import { useCallback, useContext } from "react";
 import { normalizeString } from "utils/normalizeString";
-import prod from "../../prod.json";
 
 export const useSearch = () => {
-  const { setProducts } = useContext(ProductContext);
+  const { setProducts, productsCopy } = useContext(ProductContext);
 
   const handleSearch = useCallback(
     (search: string, target: "name" | "categorie" = "name") => {
       if (search.length > 0) {
-        const filter = prod.filter((product) =>
+        const filter = productsCopy.filter((product) =>
           normalizeString(product[target].trim().toLowerCase()).includes(
             normalizeString(search.trim().toLowerCase())
           )
         );
         setProducts(filter);
       } else {
-        setProducts(prod);
+        setProducts([...productsCopy]);
       }
     },
-    []
+    [productsCopy]
   );
 
   return {
