e30123f8df9c6f57297ccbb8ba0446829bee0d1a
Williams25 <william.souza.timao@hotmail.com>
2022-06-16 10:59:50 -0300

    adicionando link para vizualização do produto

--- src/components/ProductSearchCard/index.tsx
+++ src/components/ProductSearchCard/index.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Products } from "types/Products";
 import { normalizeString } from "utils/normalizeString";
 import styled from "./styles.module.scss";
+import { encoded } from "utils/base64";
 
 export type ProductSearchCardProps = {
   product: Products;
@@ -15,48 +17,55 @@ export const ProductSearchCard = ({
   type
 }: ProductSearchCardProps) => {
   return (
-    <div className={styled.containerProductSearch}>
-      <div className={styled.boxImageSarch}>
-        <Image src={product.image} alt={product.name} width={64} height={64} />
-      </div>
+    <Link href={`/produto/${encoded(product.id)}`}>
+      <a className={styled.containerProductSearch}>
+        <div className={styled.boxImageSarch}>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={64}
+            height={64}
+          />
+        </div>
 
-      <div className={styled.infoProductSearch}>
-        <h1>
-          {product.name.split("").map((item, index) => (
-            <span
-              key={index}
-              className={
-                type === "name" &&
-                normalizeString(search?.trim().toLowerCase() || "").includes(
-                  normalizeString(item.trim().toLowerCase())
-                )
-                  ? styled.activeLetter
-                  : ""
-              }
-            >
-              {item}
-            </span>
-          ))}
-        </h1>
+        <div className={styled.infoProductSearch}>
+          <h1>
+            {product.name.split("").map((item, index) => (
+              <span
+                key={index}
+                className={
+                  type === "name" &&
+                  normalizeString(search?.trim().toLowerCase() || "").includes(
+                    normalizeString(item.trim().toLowerCase())
+                  )
+                    ? styled.activeLetter
+                    : ""
+                }
+              >
+                {item}
+              </span>
+            ))}
+          </h1>
 
-        <h2>
-          {product.categorie.split("").map((item, index) => (
-            <span
-              key={index}
-              className={
-                type === "categorie" &&
-                normalizeString(search?.trim().toLowerCase() || "").includes(
-                  normalizeString(item.trim().toLowerCase())
-                )
-                  ? styled.activeLetter
-                  : ""
-              }
-            >
-              {item}
-            </span>
-          ))}
-        </h2>
-      </div>
-    </div>
+          <h2>
+            {product.categorie.split("").map((item, index) => (
+              <span
+                key={index}
+                className={
+                  type === "categorie" &&
+                  normalizeString(search?.trim().toLowerCase() || "").includes(
+                    normalizeString(item.trim().toLowerCase())
+                  )
+                    ? styled.activeLetter
+                    : ""
+                }
+              >
+                {item}
+              </span>
+            ))}
+          </h2>
+        </div>
+      </a>
+    </Link>
   );
 };
