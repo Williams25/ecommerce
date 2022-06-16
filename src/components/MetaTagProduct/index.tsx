34e3acb0b9dae57ae8e50dbce8e833a71181006c
Williams25 <william.souza.timao@hotmail.com>
2022-06-16 11:52:39 -0300

    adicionando mais matetags

--- src/components/MetaTagProduct/index.tsx
+++ src/components/MetaTagProduct/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Products } from "types/Products";
+import { encoded } from "utils/base64";
 
 export type MetaTagProductProps = {
   product: Products;
@@ -20,6 +21,9 @@ export const MetaTagProduct = ({ product }: MetaTagProductProps) => {
       <meta property="product:brand" content={product.categorie} />
       <meta property="product:price:amount" content={String(product.price)} />
       <meta property="product:retailer_item_id" content={product.id} />
+      <meta property="og:description" content={product.description} />
+      <meta property="og:title" content={product.name} />
+      <meta property="og:locale" content={`/produto/${encoded(product.id)}`} />
     </Head>
   );
 };
