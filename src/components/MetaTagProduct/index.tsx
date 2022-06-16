25a9b3e1743eddb8d46d6b07618f71b37b417af2
Williams25 <william.souza.timao@hotmail.com>
2022-06-16 11:54:24 -0300

    ordenando metatags

--- src/components/MetaTagProduct/index.tsx
+++ src/components/MetaTagProduct/index.tsx
@@ -13,17 +13,17 @@ export const MetaTagProduct = ({ product }: MetaTagProductProps) => {
         property="og:url"
         content={`https://desafio-osf.williams25.vercel.app/`}
       />
+      <meta property="og:description" content={product.description} />
+      <meta property="og:title" content={product.name} />
+      <meta property="og:locale" content={`/produto/${encoded(product.id)}`} />
+      <meta property="og:image" content={product.image} />
       <meta property="product:availability" content="in stock" />
       <meta property="product:condition" content="new" />
       <meta property="product:price:currency" content="BRL" />
       <meta property="product:availability" content="in stock" />
-      <meta property="og:image" content={product.image} />
       <meta property="product:brand" content={product.categorie} />
       <meta property="product:price:amount" content={String(product.price)} />
       <meta property="product:retailer_item_id" content={product.id} />
-      <meta property="og:description" content={product.description} />
-      <meta property="og:title" content={product.name} />
-      <meta property="og:locale" content={`/produto/${encoded(product.id)}`} />
     </Head>
   );
 };
